32440169ef903ab755e35b6c6b18d47814917d1a
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-04-03 19:10:40 -0400

    update jeffrey

--- focus_app/src/app/components/LoginScreen.js
+++ focus_app/src/app/components/LoginScreen.js
@@ -3,16 +3,18 @@ import styles from "./LoginScreen.module.css";
 
 export const LoginScreen = () => {
   return (
-    <div className="container">
-      <div className="card">
+    <div className={styles.container}>
+      <div className="welcome">
         <img src="./ff_logo.png" alt="FocusFlow Logo" className="logo" />
         <h2>Sign In</h2>
-        <form action="#">
-          <input type="text" name="username" id="username" placeholder="Username" required />
-          <input type="password" name="password" id="password" placeholder="Password" required />
-          <button type="submit">Sign In</button>
-        </form>
       </div>
+      <div className="form">
+        <form action="#">
+            <input type="text" name="username" id="username" placeholder="Username" required />
+            <input type="password" name="password" id="password" placeholder="Password" required />
+            <button type="submit">Sign In</button>
+            </form>
+        </div>
     </div>
   );
 };
