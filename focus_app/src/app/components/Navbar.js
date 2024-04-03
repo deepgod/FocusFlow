8a84a48f913493a0e056488b7eab558f19291423
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-04-03 16:25:40 -0400

    added a navbar component

---
+++ focus_app/src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <Image src="/ff_logo.png" alt="FocusFlow logo" width={40} height={40} />
+        <span className={styles.navbar_brand}>FocusFlow</span>
+      </Link>
+    </nav>
+  );
+};
--- focus_app/src/app/page.js
+++ focus_app/src/app/page.js
@@ -1,5 +1,13 @@
+'use client'
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import styles from './page.module.css';
+
 export default function Home() {
   return (
-    <h1>Test2</h1>
+    <div> 
+      <Navbar />
+      <h1>Test2</h1>
+    </div>
   );
 }
