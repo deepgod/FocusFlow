ae2a02b29a9e62aa77e9cd9cd1bbaaae4e44e4bb
Shruti Chari <shrutichari4@gmail.com>
2024-04-03 19:25:09 -0400

    Update AddItem.js

--- focus_app/src/app/components/AddItem.js
+++ focus_app/src/app/components/AddItem.js
@@ -1,41 +1,28 @@
 import React from "react";
-import styles from "./AddItem.module.css";
+import styles from "./AddItem.css";
 
-var container = document.createElement("div");
+export const AddItem = () => {
+  return (
+    <div>
+      {/* Title component */}
+      <div>
+        <label htmlFor="titleTextbox">Title:</label>
+        <input type="text" id="titleTextbox" />
+      </div>
 
-// Title component
-var titleDiv = document.createElement("div");
-var titleLabel = document.createElement("label");
-titleLabel.innerHTML = "Title:";
-var titleTextbox = document.createElement("input");
-titleTextbox.setAttribute("type", "text");
-titleTextbox.setAttribute("id", "titleTextbox");
-titleDiv.appendChild(titleLabel);
-titleDiv.appendChild(titleTextbox);
-container.appendChild(titleDiv);
+      {/* URL component */}
+      <div>
+        <label htmlFor="urlTextbox">URL:</label>
+        <input type="text" id="urlTextbox" />
+      </div>
 
-// URL component
-var urlDiv = document.createElement("div");
-var urlLabel = document.createElement("label");
-urlLabel.innerHTML = "URL:";
-var urlTextbox = document.createElement("input");
-urlTextbox.setAttribute("type", "text");
-urlTextbox.setAttribute("id", "urlTextbox");
-urlDiv.appendChild(urlLabel);
-urlDiv.appendChild(urlTextbox);
-container.appendChild(urlDiv);
+      {/* Content component */}
+      <div>
+        <label htmlFor="contentTextarea">Content:</label>
+        <textarea rows="4" cols="50" id="contentTextarea"></textarea>
+      </div>
+    </div>
+  );
+};
 
-// Content component
-var contentDiv = document.createElement("div");
-var contentLabel = document.createElement("label");
-contentLabel.innerHTML = "Content:";
-var contentTextarea = document.createElement("textarea");
-contentTextarea.setAttribute("rows", "4");
-contentTextarea.setAttribute("cols", "50");
-contentTextarea.setAttribute("id", "contentTextarea");
-contentDiv.appendChild(contentLabel);
-contentDiv.appendChild(contentTextarea);
-container.appendChild(contentDiv);
 
-// Append the container to the document body
-document.body.appendChild(container);
