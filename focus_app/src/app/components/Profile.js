d5fdcd7c5cb4b4ea618a31574aa842d2c9754942
Shruti Chari <shrutichari4@gmail.com>
2024-04-06 19:33:36 -0400

    Create Profile.js
    added profile page for user content

---
+++ focus_app/src/app/components/Profile.js
@@ -0,0 +1,15 @@
+// Profile.js
+
+import React from 'react';
+import styles from "./Profile.module.css"
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>This is the user's profile content.</p>
+    </div>
+  );
+};
+
+export default Profile;
