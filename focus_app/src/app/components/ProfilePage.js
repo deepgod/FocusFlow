9e62ff725630d2ab7ea276d5a290c99aca8542e1
Shruti Chari <shrutichari4@gmail.com>
2024-04-06 19:33:41 -0400

    Create ProfilePage.js

---
+++ focus_app/src/app/components/ProfilePage.js
@@ -0,0 +1,14 @@
+// ProfilePage.js
+
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>User Profile Page</h1>
+      <p>This is the user's profile page content.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
