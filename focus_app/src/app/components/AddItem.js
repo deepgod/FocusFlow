9bbf850afc1759f307fccf814a67b39762cb8625
Shruti Chari <shrutichari4@gmail.com>
2024-04-06 10:39:01 -0400

    Update AddItem.js

--- focus_app/src/app/components/AddItem.js
+++ focus_app/src/app/components/AddItem.js
@@ -1,9 +1,35 @@
+/*
 import React from "react";
-import styles from "./AddItem.module.css";
+import styles from './AddItem.module.css';
 
 const AddItem = () => {
   return (
     <div className="addItemContainer">
+      <div>
+        <label htmlFor="titleTextbox">Title:</label>
+        <input type="text" id="titleTextbox" />
+      </div>
+      <div>
+        <label htmlFor="urlTextbox">URL:</label>
+        <input type="text" id="urlTextbox" />
+      </div>
+      <div>
+        <label htmlFor="contentTextarea">Content:</label>
+        <textarea rows="4" cols="50" id="contentTextarea"></textarea>
+      </div>
+      <button>Create</button>
+    </div>  
+  );
+};
+ export default AddItem;
+ */
+
+ import React from "react";
+import styles from './AddItem.module.css';
+
+const AddItem = () => {
+  return (
+    <div className={styles.addItemContainer}>
       {/* Title component */}
       <div>
         <label htmlFor="titleTextbox">Title:</label>
