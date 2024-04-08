07423976bf56fae450cba82b85abf585ad1a17f2
Shruti Chari <shrutichari4@gmail.com>
2024-04-08 17:11:59 -0400

    Update page.js

--- focus_app/src/app/sign-in/page.js
+++ focus_app/src/app/sign-in/page.js
@@ -3,8 +3,7 @@ import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import LoginScreen from '../components/LoginScreen';
 import styles from './page.css'
-import Profile from '../components/Profile';
-import ProfilePage from '../components/ProfilePage';
+
 
 export default function Home() {
   // Adds a class to the body element when the component mounts
