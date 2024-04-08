0712919fe89b465338c9bf373db9abc9f59b7627
Shruti Chari <shrutichari4@gmail.com>
2024-04-08 17:17:12 -0400

    Update ButtonList.js

--- focus_app/src/app/components/ButtonList.js
+++ focus_app/src/app/components/ButtonList.js
@@ -5,7 +5,7 @@ import Button from "./Button";
 import styles from "./ButtonList.module.css";
 
 const ButtonList = () => {
-  //const router = useRouter();
+  const router = useRouter();
   const [buttons, setButtons] = useState([
     { id: 1, label: 'Add', color: '#00a82d', imageUrl: '/x_pic.svg', onClick: () => handlePathButtonClick('/create-item') },
     { id: 2, label: 'Home', onClick: () => console.log('Home button clicked') },
