80792da05ba9538c6a4ec32022237e0c030d35cc
Shruti Chari <shrutichari4@gmail.com>
2024-04-08 17:11:57 -0400

    Update ButtonList.js

--- focus_app/src/app/components/ButtonList.js
+++ focus_app/src/app/components/ButtonList.js
@@ -1,10 +1,11 @@
+
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Button from "./Button";
 import styles from "./ButtonList.module.css";
 
 const ButtonList = () => {
-  const router = useRouter();
+  //const router = useRouter();
   const [buttons, setButtons] = useState([
     { id: 1, label: 'Add', color: '#00a82d', imageUrl: '/x_pic.svg', onClick: () => handlePathButtonClick('/create-item') },
     { id: 2, label: 'Home', onClick: () => console.log('Home button clicked') },
@@ -30,40 +31,3 @@ const ButtonList = () => {
 };
 
 export default ButtonList;
-
-/*
-import React from "react";
-//import { useRouter } from "next/router";
-import Button from "./Button";
-import styles from "./ButtonList.module.css";
-import { useRouter } from "next/navigation";
-
-const ButtonList = () => {
-  const router = useRouter();
-
-  const handlePathButtonClick = (path) => {
-    router.push(path);
-  };
-
-  return (
-    <div className={styles.buttonList}>
-      <Button
-        label="Add"
-        color="#00a82d"
-        imageUrl="/x_pic.svg"
-        onClick={() => handlePathButtonClick('/create-item')}
-      />
-      <Button
-        label="Home"
-        onClick={() => console.log('Home button clicked')}
-      />
-      <Button
-        label="Profile"
-        onClick={() => handlePathButtonClick('/profile')}
-      />
-    </div>
-  );
-};
-
-export default ButtonList;
-*/
