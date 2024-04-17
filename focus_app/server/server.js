28d0af4bb1085c772b19fc6404032dec253ca9c2
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-04-17 15:08:12 -0400

    Set up server code & update mongoose version in package-lock.json and package.json

---
+++ focus_app/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({ origin: true, credentials: true }));
+
+const conn_str = 'mongodb+srv://webprogproject4:webprogproj4@cluster0.pm6xxz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(conn_str).then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+    console.log('MongoDB connected');
+})
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+nextApp.prepare().then(() => {
+  app.get('*', (req, res) => {
+    return handle(req, res);
+  });
+}).catch((error) => {
+  console.error('Error starting Next.js server:', error);
+  process.exit(1);
+});
+
+module.exports = app;
