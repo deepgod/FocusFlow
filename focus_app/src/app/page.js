9b5dda6f43422f545e1caf28064314932e2791a1
anjalidevarapalli <ald59976@uga.edu>
2024-04-28 17:41:58 -0400

    Took out get started button

--- focus_app/src/app/page.js
+++ focus_app/src/app/page.js
@@ -14,9 +14,6 @@ export default function Home() {
             <p className={styles.description}>
               Your ultimate note-taking app for staying organized, focused, and productive.
             </p>
-            <a href="/home" className={styles.button}>
-              Get Started
-            </a>
           </div>
           <div className={styles.image}>
             <img src="/personimg.png" alt="Busy Person" />
