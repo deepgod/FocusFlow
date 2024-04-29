dceccde55f2fe12bc1ef72862e4a8016d2534da8
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-04-29 19:31:51 -0400

    Update UserContext.js and ButtonList.js files

--- focus_app/src/app/components/ButtonList.js
+++ focus_app/src/app/components/ButtonList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Button from "./Button";
 import styles from "./ButtonList.module.css";
+import { useAuth } from "./authContext";
 
 const ButtonList = ({ buttons, onSelectItem, onAddItemClick, onDeleteItem, onEditItem }) => {
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
   const handleButtonClick = (button) => {
     if (button.onClick) {
       button.onClick();
@@ -13,33 +15,37 @@ const ButtonList = ({ buttons, onSelectItem, onAddItemClick, onDeleteItem, onEdi
   };
 
   return (
-    <div className={styles.buttonList}>
-      <div key={0} onClick={onAddItemClick}>
-        <Button label="Add" color="#00a82d" imageUrl="/plus.svg" />
-      </div>
-      {buttons.map(button => (
-        <div key={button.id}>
-          <Button
-            label={button.label} 
-            color={button.color}
-            imageUrl={button.imageUrl}
-            onClick={() => handleButtonClick(button)}
-          />
-          <button 
-            className={styles.deleteButton} 
-            onClick={() => onDeleteItem(button.id)}
-          >
-            <img src="/delete.svg"/>
-          </button>
-          <button 
-            className={styles.editButton} 
-            onClick={() => onEditItem(button)}
-          >
-            <img src="/edit.svg"/>
-          </button>
+    (isLoggedIn ? (
+      <div className={styles.buttonList}>
+        <div key={0} onClick={onAddItemClick}>
+          <Button label="Add" color="#00a82d" imageUrl="/plus.svg" />
         </div>
-      ))}
-    </div>
+        {buttons.map(button => (
+          <div key={button.id}>
+            <Button
+              label={button.label} 
+              color={button.color}
+              imageUrl={button.imageUrl}
+              onClick={() => handleButtonClick(button)}
+            />
+            <button 
+              className={styles.deleteButton} 
+              onClick={() => onDeleteItem(button.id)}
+            >
+              <img src="/delete.svg"/>
+            </button>
+            <button 
+              className={styles.editButton} 
+              onClick={() => onEditItem(button)}
+            >
+              <img src="/edit.svg"/>
+            </button>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <p>Please Sign-In to continue</p>
+    ))
   );
 };
 
